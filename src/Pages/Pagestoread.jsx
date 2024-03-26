fbd7f4b551a5fb1439f4b10450ecf1f8b028b3a2
Farhad2590 <farhadhossen2590@gmail.com>
2024-03-27 05:19:48 +0600

    Graph Chart Added

--- src/Pages/Pagestoread.jsx
+++ src/Pages/Pagestoread.jsx
@@ -1,8 +1,50 @@
+import  { useEffect, useState } from "react";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { getReadBooks } from "../Utilities/Utilities";
+
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
+const TriangleBar = (props) => {
+  const { fill, x, y, width, height } = props;
+
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+    Z`;
+  };
+
+  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
 
 const Pagestoread = () => {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        const storedBooks = getReadBooks();
+        setBooks(storedBooks)
+    }, [])
+
+    const chartData = books.map(book => ({
+        name: `${book.bookName} - Page ${book.pageNumber}`,
+        pages: book.totalPages
+    }));
+    
     return (
         <div>
-            <h2>Hello From Pages to read</h2>
+            <h2>Bar Chart of Book Ratings</h2>
+            <BarChart width={700} height={500} data={chartData} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="pages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                    {chartData.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+                    ))}
+                </Bar>
+            </BarChart>
         </div>
     );
 };
