4c6cc91e18e31e52a8c4687915bdcc16d36edfb0
Farhad2590 <farhadhossen2590@gmail.com>
2024-03-27 07:13:15 +0600

    Some Responsive added

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -2,19 +2,25 @@ import { NavLink } from "react-router-dom";
 
 const Nav = () => {
     return (
-        <div className="flex justify-between items-center bg-base-100 pt-5 pb-5">
-            <div className="">
+        <div className="flex flex-col md:flex-row justify-between items-center bg-base-100 pt-5 pb-5 px-5 md:px-10">
+            <div className="flex items-center">
                 <a className="btn btn-ghost text-xl">Book Vibe</a>
             </div>
-            <div className="flex gap-5">
-                <NavLink to={'/'} className={({isActive}) => isActive ? 'text-green-500 font-bold btn btn-outline' : 'btn btn-outline-none bg-white'}>Home</NavLink>
-                <NavLink to={'/listedBooks'} className={({isActive}) => isActive ? 'text-green-500 font-bold btn btn-outline' : 'btn btn-outline-none bg-white'}>Listed Books</NavLink>
-                <NavLink to={'/pagestoread'} className={({isActive}) => isActive ? 'text-green-500 font-bold btn btn-outline' : 'btn btn-outline-none bg-white'}>Pages To read</NavLink>
-                <NavLink to={'/deliveryInfo'} className={({isActive}) => isActive ? 'text-green-500 font-bold btn btn-outline' : 'btn btn-outline-none bg-white'}>Delivery Info</NavLink>
-
-
+            <div className="flex flex-col md:flex-row gap-5 mt-3 md:mt-0">
+                <NavLink exact to={'/'} activeClassName="text-green-500 font-bold btn btn-outline" className="btn btn-outline-none bg-white">
+                    Home
+                </NavLink>
+                <NavLink to={'/listedBooks'} activeClassName="text-green-500 font-bold btn btn-outline" className="btn btn-outline-none bg-white">
+                    Listed Books
+                </NavLink>
+                <NavLink to={'/pagestoread'} activeClassName="text-green-500 font-bold btn btn-outline" className="btn btn-outline-none bg-white">
+                    Pages To read
+                </NavLink>
+                <NavLink to={'/deliveryInfo'} activeClassName="text-green-500 font-bold btn btn-outline" className="btn btn-outline-none bg-white">
+                    Delivery Info
+                </NavLink>
             </div>
-            <div className="flex gap-3">
+            <div className="flex gap-3 mt-3 md:mt-0">
                 <a className="btn bg-green-500 text-white">Sign In</a>
                 <a className="btn bg-blue-400 text-white">Sign Up</a>
             </div>
